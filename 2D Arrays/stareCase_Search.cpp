d68578aca7d32ce07d7c185166f9a141ff2b7eba
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-02-23 00:22:41 +0530

    Update stareCase_Search.cpp

--- 2D Arrays/stareCase_Search.cpp
+++ 2D Arrays/stareCase_Search.cpp
@@ -16,6 +16,23 @@ bool stareCase(int matrix[4][4], int n , int m , int target){
     }
     return false;
 }
+
+// starting from another index
+bool stareCase1(int matrix[4][4], int n , int m , int target){
+    int row = n-1, col = 0;
+    
+    while(row >= 0 && col < m){
+        if( matrix[row][col] == target){
+            return true;
+        }else if(matrix[row][col] < target) {
+            col++;
+        }else{ 
+            row--;
+        }
+    }
+    return false;
+}
+
 int main(){
     int arr[4][4] = {{1,2,3,4},
                     {5,6,7,8},
@@ -24,4 +41,6 @@ int main(){
     int n = 4, m = 4;
     int key = 31;
     cout<<stareCase(arr,n,m,key);
+    cout<<stareCase1(arr,n,m,45);
+
 }
