1a6d37ae37b8d251530ca6dd65a9639c772bca82
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-03-08 13:06:54 +0530

    Add files via upload

---
+++ Char_Array_and_String/char_array.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void toUpper(char word[] , int n ){
+    n = strlen(word);
+    for(int i = 0; i < n ; i++){
+        char ch = word[i];
+        if(ch>= 'A' && ch <= 'Z'){   // upper case 
+            continue;
+        }else{                    // lower case 
+            word[i] = ch - 'a' + 'A';
+        }
+    }
+
+}
+
+int main(){
+
+    // char work[] = {'c', 'o', 'd', 'e', '\0'};
+    // //or
+    // // char work[50] = {'c', 'o', 'd', 'e', '\0'};
+    // //or
+    // // char work[4] = "code";
+    // //or
+    // // char work[] = "code";
+
+    // cout << work <<endl;
+    // cout << strlen(work);
+
+    // ....................................................
+    // input 
+    // char word[30];
+    // cin>> word;  // it ignores white spaces 
+
+    // cout<< "your word was : "<< word <<endl;
+    // cout<< "length : "<< strlen(word);
+
+    // char sentence[30];
+
+    // cin.getline(sentence,30);
+
+    // cout << "your word was : " << sentence << endl;
+    // cout << "length : " << strlen(sentence)<< endl;
+    
+    //........................................................
+
+    // convert to upper case 
+
+    char word[] = "ViKash";
+    toUpper(word, strlen(word));
+    cout<<word;
+
+    return 0;
+}
